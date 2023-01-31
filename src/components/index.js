af299576f8b4be80708de50880b8752d5f5747ec
ezequiel gallardo <ezeedge@gmail.com>
2023-01-31 02:21:18 -0300

    upload proyect

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
-export { default as AddItem} from './add-item';
-export { default as TaskList} from './task/index'
-export { default as CustomModal} from './modal/index'
+export { default as Header } from "./header/index";
+export { default as Card } from "./card/index";
+export { default as NumberContainer } from "./number-container/index";
---
+++ src/constants/themes/colors.js
@@ -0,0 +1,9 @@
+export const colors = {
+  primary: "#f67280",
+  secondary: "#f8b195",
+  background: "#F5F5F5",
+  text: "#212121",
+  white: "#fff",
+  black: "#000",
+  card: "#f8f1e9",
+};
